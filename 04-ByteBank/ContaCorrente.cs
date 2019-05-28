6c1607c7e34e0d201bc2c9aa6d9d0414c60eea20
Arthur teixeira <stag006@benner.com.br>
2019-05-28 12:07:31 -0300

    ultima alteração

--- 04-ByteBank/ContaCorrente.cs
+++ 04-ByteBank/ContaCorrente.cs
@@ -15,11 +15,9 @@ public class ContaCorrente
         {
             return false;
         }
-        else
-        {
+        
             this.saldo -= valor;
-            return true;
-        }
+            return true;        
     }
 
     public void Depositar(double valor)
@@ -33,14 +31,20 @@ public class ContaCorrente
         {
             return false;
         }
-    
+
         this.saldo -= valor;
         contaDestino.Depositar(valor);
         return true;
-            
-        
     }
-
-
-
 }
+
+
+
+/* APRENDEMOS:
+ * como definir métodos com parâmetros e retorno;
+como retornar algo usando a palavra chave return antes da expressão do resultado;
+o uso do return para retornar antecipadamente a execução de um método void;
+como usar a referência this para acessar um atributo;
+que podemos passar uma referência como parâmetro do método;
+métodos são chamadas a partir da referência usando o operador ...
+ */
